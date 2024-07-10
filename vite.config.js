2b360318bf2ac7ba08d4eab3fadf78f5510d46d1
root <root@srv558367.hstgr.cloud>
2024-07-10 21:56:35 +0000

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+<<<<<<< Updated upstream
   plugins: [react()],
 })
+=======
+ server: {
+    host: '0.0.0.0', // This makes the server accessible externally
+    port: 5173, // You can keep this or change to your preferred port
+  },
+	plugins: [react()],
+})
+>>>>>>> Stashed changes
