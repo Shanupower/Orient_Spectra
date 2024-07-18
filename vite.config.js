d04580463414e4315a51221ecc0b35709c64af6b
Tariq Ali <tarikali7444@gmail.com>
2024-07-18 15:28:36 +0530

    error solve

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 5173, // Default port
-    open: true, // Automatically open the app in the browser
-  },
-  build: {
-    outDir: 'dist', // Output directory for build files
-  },
-});
+  plugins: [react()],
+})
