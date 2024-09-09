95d246d9a9ed717c269bd1c8b134e45a1133eaf7
Tariq Ali <tarikali7444@gmail.com>
2024-09-09 15:29:23 +0530

    changes

--- src/Landing_page/Events.jsx
+++ src/Landing_page/Events.jsx
@@ -1,7 +1,13 @@
 import { motion } from "framer-motion";
 import axios from "axios";
-import { useState, useEffect } from "react";
 import EventDargCard from "./EventSwip";
+import React, { useRef, useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper/modules";
 import "./index.css";
 
 const Events = () => {
@@ -17,9 +23,8 @@ const Events = () => {
       );
       if (response?.status === 200) {
         setUpcominngEventData(response?.data.data);
-        
       }
-    } catch (error) {   
+    } catch (error) {
       console.log("ERROR OCCURED WHILE FETCHING:", error.message);
     }
   };
@@ -29,7 +34,6 @@ const Events = () => {
         `https://strapi.orientspectra.com/api/events?filters[Date_of_the_event][$lt]=${currentDate}?sort[0]=Date_of_the_event&populate=*`
       );
       if (response?.status === 200) {
-      
         setCompletedEventData(response?.data.data);
       }
     } catch (error) {
@@ -44,39 +48,78 @@ const Events = () => {
 
   return (
     <div className="section Events__container">
-      <motion.div
+      <Swiper
+        cssMode={true}
+        navigation={true}
+        pagination={true}
+        mousewheel={true}
+        keyboard={true}
+        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
         className="event_leftSide"
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: true, amount: 0.5 }}
-        transition={{ duration: 0.8 }}
-        variants={{
-          hidden: { opacity: 0, y: -200 },
-          visible: { opacity: 1, y: 0 },
-        }}
       >
-        <div className="event_leftContent">
-          <div className="textBox">
-          <b className="upcomingText">Upcoming Events</b></div>
-          <div className="dateEvent">
-            <p>Date of the event</p>
-            <span>{upcomingEventdata[0]?.attributes?.Date_of_the_event}</span>
+        {upcomingEventdata?.map((e, index) => (
+          <SwiperSlide key={index}>
+            <div className="swiperLeftSide">
+              <div className="event_leftContent">
+                <div className="textBox">
+                  <b className="upcomingText">Upcoming Events</b>
+                </div>
+                <div className="dateEvent">
+                  <p>Date of the event</p>
+                  <span>{e?.attributes?.Date_of_the_event}</span>
+                </div>
+                <div className="event_bottomContent">
+                  <h4 className="explaintag">{e?.attributes?.Headline}</h4>
+                  <span>{e?.attributes?.Shortdescription}</span>
+                </div>
+              </div>
+              <div className="eventsRigtImage">
+                <b className="upcomingEvents">Upcoming Events</b>
+                <img
+                  src={`https://strapi.orientspectra.com${e?.attributes?.Poster?.data?.attributes.url}`}
+                  alt=""
+                />
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+
+        {/* <motion.div
+          className="event_leftSide"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.8 }}
+          variants={{
+            hidden: { opacity: 0, y: -200 },
+            visible: { opacity: 1, y: 0 },
+          }}
+        >
+          <div className="event_leftContent">
+            <div className="textBox">
+              <b className="upcomingText">Upcoming Events</b>
+            </div>
+            <div className="dateEvent">
+              <p>Date of the event</p>
+              <span>{upcomingEventdata[0]?.attributes?.Date_of_the_event}</span>
+            </div>
+            <div className="event_bottomContent">
+              <h4 className="explaintag">
+                {upcomingEventdata[0]?.attributes?.Headline}
+              </h4>
+              <span>{upcomingEventdata[0]?.attributes?.Shortdescription}</span>
+            </div>
           </div>
-          <div className="event_bottomContent">
-            <h4 className="explaintag">
-              {upcomingEventdata[0]?.attributes?.Headline}
-            </h4>
-            <span>{upcomingEventdata[0]?.attributes?.Shortdescription}</span>
+          <div className="eventsRigtImage">
+            <b className="upcomingEvents">Upcoming Events</b>
+            <img
+              src={`https://strapi.orientspectra.com${upcomingEventdata[0]?.attributes?.Poster?.data?.attributes.url}`}
+              alt=""
+            />
           </div>
-        </div>
-        <div className="eventsRigtImage">
-          <b className="upcomingEvents">Upcoming Events</b>
-          <img
-            src={`https://strapi.orientspectra.com${upcomingEventdata[0]?.attributes?.Poster?.data?.attributes.url}`}
-            alt=""
-          />
-        </div>
-      </motion.div>
+        </motion.div> */}
+      </Swiper>
+
       <motion.div
         className="event_rightSide"
         initial="hidden"
