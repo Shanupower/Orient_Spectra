97d1c84e8f968343a58400c7f5df681e1e98cd3f
MNaveen5646 <mnaveenorientspectra@gmail.com>
2024-09-16 17:43:16 +0530

    -bug fixed in whatsapp

--- src/Whatsapp.jsx
+++ src/Whatsapp.jsx
@@ -2,7 +2,7 @@ import"./index.css";
 
 const WhatsApp =() =>{
 
-    const phoneNumber = "6305077358";
+    const phoneNumber = "+916305077358";
   
     const handleClick = () => {
       const url = `https://wa.me/${phoneNumber}`;
