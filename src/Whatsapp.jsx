c70afc5e16997fa74aa6ecbfa1f81577001c3bc6
MNaveen5646 <mnaveenorientspectra@gmail.com>
2024-10-04 11:44:43 +0530

    whatsApp

--- src/Whatsapp.jsx
+++ src/Whatsapp.jsx
@@ -2,34 +2,21 @@ import { useEffect, useState } from "react";
 import"./index.css";
 
 const WhatsApp =() =>{
-    const [clickCounts, setClickCounts] = useState(0);
     const phoneNumber = "+919052891111";
-  
-    // useEffect(() => {
-    //     const storedCounts = localStorage.getItem('whatsappClickCounts'); // Get counts from localStorage
-    //     if (storedCounts) {
-    //         setClickCounts(JSON.parse(storedCounts)); // Parse and set state
-    //     }
-    // }, []);
 
 
-    const handleClick = () => {
-      const url = `https://wa.me/${phoneNumber}`;
-      window.open(url, "_blank");
-    //   const today = new Date().toISOString().split('T')[0]; // Get today's date in YYYY-MM-DD format
-    //   const newCounts = { ...clickCounts }; // Copy current counts
-
-    //   // Increment today's count or initialize it
-    //   newCounts[today] = (newCounts[today] || 0) + 1; 
-      
-    //   setClickCounts(newCounts); // Update state
-    //   localStorage.setItem('whatsappClickCounts', JSON.stringify(newCounts));
-    };
+    // const handleClick = () => {
+    //   const url = `https://wa.me/${phoneNumber}`;
+    //   window.open(url, "_blank");
+    // };
 
     return(
-        <div className="WhatsApp section" onClick={handleClick}>
+        <a href="https://wa.me/+919052891111?text" target="_blank">
+        <div className="WhatsApp section">
             <img src="https://strapi.orientspectra.com/uploads/whatsapp_9450bbb097.webp" alt="WhatsApp" loading="lazy"/>
         </div>
+        </a>
+
     )
 }
 
