c20a9d71bc9c4c1001888a7ede4cb780ba882278
MNaveen5646 <mnaveenorientspectra@gmail.com>
2024-10-07 18:22:50 +0530

    image changed

--- src/Ads landing page/Europe-Education-Fair/UK_EventDetails.jsx
+++ src/Ads landing page/Europe-Education-Fair/UK_EventDetails.jsx
@@ -9,12 +9,12 @@ const EventDetails =()=>{
                 <p>Orient Spectra provides end-to-end guidance with your study abroad goal across university connects, visa application, 
                 and loan assistance. Our student-focussed offerings and expert counselling has made us the top overseas education consultants 
                 in Hyderabad.</p>
-                <p><b>Date :</b> <span>21 September 2024</span></p>
+                <p><b>Date :</b> <span>5th October 2024</span></p>
                 <p><b>Time :</b>  <span>10 AM – 4 PM</span></p>
                 <p><b>Venue :</b> <span>Hyatt Place, Banjara Hills, Hyderabad</span></p>
                 </div>
                 <div className="Image_container">
-                <img src="https://strapi.orientspectra.com/uploads/1080_x_1080_2_f01732bd6b.jpg" alt=""/>
+                <img src="https://strapi.orientspectra.com/uploads/391_x_520_EU_Event_Banner_6605cc7233_b9c19b8262.jpg" alt=""/>
                 </div>
             </div>
             
