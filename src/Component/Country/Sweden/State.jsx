f85d762c93f80fbdc620f6bc76d84f8a5d80fef4
MNaveen5646 <mnaveenorientspectra@gmail.com>
2024-10-08 16:12:28 +0530

    common leadform component created

--- src/Component/Country/Sweden/State.jsx
+++ src/Component/Country/Sweden/State.jsx
@@ -1,92 +1,10 @@
 import "./Country.css";
 import { useState } from "react";
 import axios from "axios";
+import LeadForm from "../../Common/LeadForm";
 
 const State = () => {
-
-  const [formData, setFormData] = useState({
-    Name: "",
-    Email: "",
-    Mobile: "",
-    Intake_Year: "",
-    Source: "SWEDEN",
-  });
-
-  const [errors, setErrors] = useState({});
-
-  const handleSubmitData = async () => {
-    const api = "https://strapi.orientspectra.com/api/lead-form-hompages";
-    try {
-      const response = await axios.post(api, {
-        data: formData,
-      });
-      console.log(formData,"formdata");
-      if (response.status === 200) {
-        alert("Form submitted successfully");
-
-       
-        setFormData({
-          Name: "",
-          Email: "",
-          Mobile: "",
-          Intake_Year: "",
-          Source: "SWEDEN",
-        });
-      }
-    } catch (errors) {
-      console.log(errors);
-    }
-  };
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    if (name === "Mobile") {
-      if (/^\d*$/.test(value) && value.length <= 10) {
-        setFormData((prevFormData) => ({
-          ...prevFormData,
-          [name]: value,
-        }));
-      }
-    } else {
-      setFormData({
-        ...formData,
-        [name]: value,
-      });
-    }
-  };
-
-  const Validation = () => {
-    const newErrors = {};
-
-    if (formData.Name === "") {
-      newErrors.Name = "First Name is Required";
-    }
-    if (formData.Email === "") {
-      newErrors.Email = "Email Id is Required";
-    } else if (!/\S+@\S+\.\S+/.test(formData.Email)) {
-      newErrors.Email = "Email is not valid";
-    }
-    if (formData.Mobile === "") {
-      newErrors.Mobile = "Enter the Mobile Number";
-    } else if (!/^\d{10}$/.test(formData.Mobile)) {
-      newErrors.Mobile = "Enter the valid 10 digit mobile number";
-    }
-    if (formData.Intake_Year === "") {
-      newErrors.Intake_Year = "Intake Year is Required";
-    }
-    return newErrors;
-  };
-
-  const handleSubmitValidation = (event) => {
-    event.preventDefault();
-    const validationErrors = Validation();
-    if (Object.keys(validationErrors).length === 0) {
-      handleSubmitData();
-    } else {
-      setErrors(validationErrors);
-    }
-  };
-
+  const sourceValue ="Sweden";
 
   return (
     <div className="section">
@@ -95,64 +13,7 @@ const State = () => {
         <p>Get expert guidance to help you get closer to your dream of studying in the 
         Sweden and fastrack your visa application process.{" "}</p>
         <div className="lead-form-container">
-        <h3>Register for Personalized Counselling</h3>
-        <form onSubmit={handleSubmitValidation}>
-          <div className="text-feild container">
-          <div className="form-fields">
-            <input
-                type="text"
-                name="Name"
-                placeholder="Name"
-                className="input-fields"
-                onChange={handleChange}
-                value={formData.Name}
-              />
-              {errors.Name && <p style={{ color: "red" }}>{errors.Name}</p>}
-              </div>
-
-              <div className="form-fields">
-                <input
-                  type="text"
-                  name="Email"
-                  placeholder="Email"
-                  className="input-fields"
-                  onChange={handleChange}
-                value={formData.Email}
-                />
-                {errors.Email && <p style={{ color: "red" }}>{errors.Email}</p>}
-              </div>
-
-              <div className="form-fields">
-                <input
-                  type="text"
-                  name="Mobile"
-                  placeholder="Mobile Number"
-                  className="input-fields"
-                  onChange={handleChange}
-                value={formData.Mobile}
-                />
-                {errors.Mobile && <p style={{ color: "red" }}>{errors.Mobile}</p>}
-              </div>
-
-              <div className="form-fields">
-                <input
-                 type="text"
-                 name="Intake_Year" 
-                 placeholder="Intake Year" 
-                 className="input-fields"
-                 onChange={handleChange}
-                value={formData.Intake_Year}
-                />
-                {errors.Intake_Year && (
-                <p style={{ color: "red" }}>{errors.Intake_Year}</p>
-              )}
-              </div>
-              <input type="hidden" name="Source" className="input-fields" />
-            <button className="form-button" type="submit">
-            Book A Free Call
-          </button>
-          </div>
-        </form>
+          <LeadForm source={sourceValue} />
         </div>
       </div>
 
