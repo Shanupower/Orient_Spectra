272d2f8d740fe301a8d746804a0060037c264ded
MNaveen5646 <mnaveenorientspectra@gmail.com>
2024-11-13 11:44:05 +0530

    Whatsapp number changed

--- src/Whatsapp.jsx
+++ src/Whatsapp.jsx
@@ -4,17 +4,17 @@ import WHATSAPP from "./assets/Home_page/Whatsapp.webp"
 import PHONE from "./assets/Home_page/Phone.webp"
 
 const WhatsApp =() =>{
-    const phoneNumber = "+919281161858";
+    const phoneNumber = "+919000456765";
     return(
         <div className="ActionIcons">
         <div className="Phone">
-        <a href="tel:9281161858">
+        <a href="tel:9000456765">
                 {/* <img src="https://strapi.orientspectra.com/uploads/phone_call_1_d5d085a484.webp" alt="Phone" loading="lazy" /> */}
                 <img src={PHONE} alt="Phone" loading="lazy" />
             </a>
         </div>
         <div className="WhatsApp section">
-            <a href="https://wa.me/+919000456765?text" target="_blank">
+            <a href="https://wa.me/+919281161858?text" target="_blank">
             {/* <img src="https://strapi.orientspectra.com/uploads/whatsapp_9450bbb097.webp" alt="WhatsApp" loading="lazy"/> */}
             {/* <img src="https://strapi.orientspectra.com/uploads/whatsapp_9450bbb097.webp" alt="WhatsApp" loading="lazy"/> */}
             <img src={WHATSAPP} alt="WhatsApp" loading="lazy"/>
